58bbf5044d366525c4846619e13c72112e5f264a
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-04-10 19:19:23 -0700

    spotify serach is working

--- liri.js
+++ liri.js
@@ -5,34 +5,35 @@ var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
 
 var request = process.argv[2];
-var item = process.argv.slice(3).join(" ");
+var itemSearch = process.argv.slice(3).join(" ");
 
-switch (request) {
-  case "concert-this":
-    getArtist(item);
+function runLiri(request, itemSearch) {
+  switch (request) {
+    case "concert-this":
+      getArtist(itemSearch);
 
-    break;
+      break;
 
-  case "spotify-this-song":
-    getSong(item);
+    case "spotify-this-song":
+      getSong(itemSearch);
 
-    break;
+      break;
 
-  case "movie-this":
-    getMovie(item);
+    case "movie-this":
+      getMovie(itemSearch);
 
-    break;
+      break;
 
-  case "do-what-it-says":
-    getWhatItSays(item);
+    case "do-what-it-says":
+      getWhatItSays(itemSearch);
 
-    break;
+      break;
 
-  default:
-    break;
+    default:
+      break;
+  }
 }
 
 function getArtist(artist) {
@@ -56,7 +57,24 @@ function getArtist(artist) {
     });
 }
 
-function getSong(song) {}
+function getSong(song) {
+  var spotify = new Spotify(keys.spotify);
+
+  if (song === "") {
+    song = "The Sign";
+  }
+
+  spotify.search({ type: "track", query: song }, function (error, data) {
+    if (error) {
+      console.log("Error: " + error);
+    }
+
+    console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Preview Link of Song: " + data.tracks.items[0].href);
+    console.log("Album: " + data.tracks.items[0].album.name);
+  });
+}
 
 function getMovie(movie) {
   axios
@@ -76,3 +94,5 @@ function getMovie(movie) {
 }
 
 function getWhatItSays(says) {}
+
+runLiri(request, itemSearch);
